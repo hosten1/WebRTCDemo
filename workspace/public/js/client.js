80afc5575cabe8ee7026843752f6b43c184f78ef
luoyongmeng <hosten_lym@163.com>
2022-04-17 20:58:47 +0800

    增加socketio 及聊天功能

--- workspace/public/js/client.js
+++ workspace/public/js/client.js
@@ -28,6 +28,20 @@ const downloadBtn = document.getElementById("download_Btn");
 
 // 获取显示的div
 const showDiv =  document.getElementById("constraints");
+
+// 房间聊天功能
+var userName = document.querySelector('input#username');
+var inputRoom = document.querySelector('input#room');
+var btnConnect = document.querySelector('button#connect');
+var btnLeave = document.querySelector('button#leave');
+var outputArea = document.querySelector('textarea#output');
+var inputArea = document.querySelector('textarea#input');
+var btnSend = document.querySelector('button#send');
+
+var socket;
+var room;
+
+
 // 防止重复去获取设备列表
 var isGet = false;
 function start(){
@@ -122,7 +136,8 @@ async function startRecord(){
       console.log("recorder started");
       recoderVideoShow.style.background = "red";
       recoderVideoShow.style.color = "black";
-
+      recoderVideoShow.stop = true;
+      recoderVideoShow.src = 'none';
       buffer = [];
     var  option  = {
         mimeType:'video/webm;codecs=vp8'
@@ -215,4 +230,66 @@ downloadBtn.onclick = async ()=>{
     a.download = 'aaa.webm';
     a.click();
 
+}
+
+btnConnect.onclick = ()=>{
+
+	//connect
+	socket = io.connect(); 
+	
+	//recieve message
+	socket.on('joined', (room, id) => {
+		btnConnect.disabled = true;
+		btnLeave.disabled = false;
+		inputArea.disabled = false;
+		btnSend.disabled = false;
+	});	
+	
+	socket.on('leaved', (room, id) => {
+		btnConnect.disabled = false;
+		btnLeave.disabled = true;
+		inputArea.disabled = true;
+		btnSend.disabled = true;
+
+		socket.disconnect();
+	});	
+
+	socket.on('message', (room, id, data) => {
+		outputArea.scrollTop = outputArea.scrollHeight;//窗口总是显示最后的内容
+		outputArea.value = outputArea.value + data + '\r';
+	});	
+
+	socket.on('disconnect', (socket)=>{
+		btnConnect.disabled = false;
+		btnLeave.disabled = true;
+		inputArea.disabled = true;
+		btnSend.disabled = true;
+	});
+
+	//send message
+	room = inputRoom.value;
+	socket.emit('join', room);
+}
+
+btnSend.onclick = ()=>{
+	var data = inputArea.value;
+	data = userName.value + ':' + data;
+	socket.emit('message', room, data);
+	inputArea.value = '';
+}
+
+btnLeave.onclick = ()=>{
+	room = inputRoom.value;
+	socket.emit('leave', room);
+}
+
+inputArea.onkeypress = (event)=> {
+    //event = event || window.event;
+    if (event.keyCode == 13) { //回车发送消息
+	var data = inputArea.value;
+	data = userName.value + ':' + data;
+	socket.emit('message', room, data);
+	inputArea.value = '';
+	event.preventDefault();//阻止默认行为
+    }
 }
--- workspace/sever.js
+++ workspace/sever.js
@@ -19,15 +19,72 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/public/index.html');
 });
-//将 socket.io 绑定到服务器上，于是任何连接到该服务器的客户端都具备了实时通信功能。
+
 const httpServer = https.createServer(app);
 
 httpServer.listen(80, '0.0.0.0', () => {
     console.log('httpServer running on port: ', 80);
 });
-//将 socket.io 绑定到服务器上，于是任何连接到该服务器的客户端都具备了实时通信功能。
 const httpsServer = https.createServer(options, app);
 
 httpsServer.listen(443, '0.0.0.0', () => {
     console.log('httpsServer running on port: ', 443);
+});
+//将 socket.io 绑定到服务器上，于是任何连接到该服务器的客户端都具备了实时通信功能。
+const io = require('socket.io')(httpsServer);
+//服务器监听所有客户端，并返回该新连接对象
+io.on('connection', function (socket) {
+    console.log('===>connection request query= ' + JSON.stringify(socket.handshake.query) );
+    socket.on('join', (room) => {
+        console.log("====>join" + JSON.stringify(room));
+        //加入io的到房间里
+        socket.join(room);
+        // 获取socket的房间
+        var myRoom = io.sockets.adapter.rooms[room];
+        var users = 0;
+        if (myRoom) {
+            users =  Object.keys(myRoom.sockets).length;
+        }
+       socket.emit('joined',room,socket.id);
+    // 给除了自己之外的所有人发
+    //    socket.to(room).emit('joined',room,socket.id);
+    // 给房间里所有人发
+        // io.in(room).emit('joined',room,socket.id);
+        // socket.broadcast.emit('joined',room,socket.id);
+    });
+    socket.on('leave', (room) => {
+        console.log("====>leave" + JSON.stringify(request));
+         // 获取socket的房间
+         var myRoom = io.sockets.adapter.rooms[room];
+         var users = 0;
+         if (myRoom) {
+             users =  Object.keys(myRoom.sockets).length;
+         }
+        //加入io的到房间里
+        socket.leave(room);
+       
+    //    socket.emit('joined',room,socket.id);
+    // 给除了自己之外的所有人发
+    //    io.to(room).emit('joined',room,socket.id);
+    // 给房间里所有人发
+        // socket.in(room).emit('joined',room,socket.id);
+        socket.broadcast.emit('leave',room,socket.id);
+    });
+    socket.on('message', (room,data) => {
+        console.log("====>message" + JSON.stringify(data));
+        io.in(room).emit('message', room, socket.id, data)//房间内所有人
+    });
+    socket.on('leave', (room)=> {
+		var myRoom = sockio.sockets.adapter.rooms[room];
+		var users = Object.keys(myRoom.sockets).length;
+		//users - 1;
+
+		logger.log('the number of user in room is: ' + (users-1));
+
+		socket.leave(room);
+	 	socket.emit('leaved', room, socket.id);	
+	 	//socket.to(room).emit('joined', room, socket.id);//除自己之外
+		//io.in(room).emit('joined', room, socket.id)//房间内所有人
+	 	//socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点	
+	});
 });
